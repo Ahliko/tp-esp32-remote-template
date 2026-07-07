891992124e8f95fc0179ff858a75da6b2e535962
Ahliko <killian.guillemot@ynov.com>
2026-07-07 09:46:47 +0200

    move BLE define in config.h

--- src/LowWare/BLE/BLEManagerLow.cpp
+++ src/LowWare/BLE/BLEManagerLow.cpp
@@ -1,17 +1,6 @@
 #include "BLEManagerLow.h"
 #include <Arduino.h>
 
-// UUIDs
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHAR_CURRENT_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-#define CHAR_PCB_TEMP_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a9"
-#define CHAR_AMB1_TEMP_UUID "beb5483e-36e1-4688-b7f5-ea07361b26aa"
-#define CHAR_AMB2_TEMP_UUID "beb5483e-36e1-4688-b7f5-ea07361b26ab"
-#define CHAR_ALARM_STAT_UUID "beb5483e-36e1-4688-b7f5-ea07361b26ac"
-#define CHAR_LOGS_UUID "beb5483e-36e1-4688-b7f5-ea07361b26ad"
-#define CHAR_CFG_MAX_CURRENT_UUID "beb5483e-36e1-4688-b7f5-ea07361b26ae"
-#define CHAR_CFG_MAX_TEMP_PCB_UUID "beb5483e-36e1-4688-b7f5-ea07361b26af"
-#define CHAR_CFG_MAX_TEMP_AMB_UUID "beb5483e-36e1-4688-b7f5-ea07361b26b0"
 
 BLEManagerLow::BLEManagerLow() : _server(nullptr), _isConnected(false) {
     _currentConfig = { LimitConfig(), false };
